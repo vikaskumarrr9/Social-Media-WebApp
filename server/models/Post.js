4b749394d68f805716de74eeb4009e93f62630b4
vikaskumarrr9 <vikaskumarrr9@gmail.com>
2024-12-03 00:52:08 +0530

    Social Media WebApp

---
+++ server/models/Post.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String, // Added field to store the picture path of the logged-in user
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
